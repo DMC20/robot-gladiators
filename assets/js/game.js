a02797e613792376919458d51099b01ffbcafddf
Daniel Carazo <danielcarazo20@gmail.com>
2021-06-05 12:58:16 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,22 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // you can also log mulitple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 100;
+var enemyAttack = 10;
 
-var playerMoney = 10;
+function fight();
 
-var fight = function() {
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+var fight = function(enemyNames) {
     // alert players that they are starting the round
     window.alert("Welcome to the Robot Gladiators!");
 
@@ -26,14 +31,14 @@ console.log(promptFight);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+        playerName + " attacked " +  + ". " +  + " now has " + enemyHealth + " health remaining. "
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died");
+        window.alert( + " has died");
     } else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+        window.alert( + " still has " + enemyHealth + " health left. ");
     }
 
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
@@ -41,7 +46,7 @@ console.log(promptFight);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+         + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -72,5 +77,3 @@ console.log(promptFight);
         window.alert("You need to chose a valid option. Try again!");
     }
 };
-
-fight();
